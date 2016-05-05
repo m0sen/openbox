bbb5dc7795df4caa241c0dc72a32a7e0aad10b64
Mohsen Muhammadi Shayan <m0sen@users.noreply.github.com>
2016-05-05 17:29:07 +0430

    Added files via upload

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,11 @@
-nitrogen --restore &
+sh ~/.fehbg &
 setxkbmap us,ir -option "lv3:ralt_alt,grp:alt_shift_toggle" &
+numlockx &
 (sleep 1s && tint2) &
 (sleep 1s && xcompmgr) &
 (sleep 2s && volumeicon) &
 (sleep 2s /usr/bin/pulseaudio --start --log-target=syslog) &
 (sleep 3s && plank) &
 (sleep 5s && uget-gtk) &
+(sleep 5s && xcalib -d :0 /usr/share/color/icc/colord/Retina\ MBP\ screen.icc) &
+(sleep 5s && Tumblerd.sh) &
