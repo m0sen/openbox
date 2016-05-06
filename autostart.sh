1d6baa21ba97fb33a4032a6561578ccce71fb235
Mohsen Muhammadi Shayan <m0sen@users.noreply.github.com>
2016-05-06 11:06:54 +0430

    Added files via upload

--- autostart.sh
+++ autostart.sh
@@ -2,8 +2,8 @@ sh ~/.fehbg &
 setxkbmap us,ir -option "lv3:ralt_alt,grp:alt_shift_toggle" &
 numlockx &
 (sleep 1s && tint2) &
-(sleep 1s && xcompmgr) &
-(sleep 2s && volumeicon) &
+(sleep 1s && compton -CGb) &
+(sleep 2s && pnmixer) &
 (sleep 2s /usr/bin/pulseaudio --start --log-target=syslog) &
 (sleep 3s && plank) &
 (sleep 5s && uget-gtk) &
